3926bd2b0af1fbf0231589974082abdf43d37dea
thecat58 <mkike518@gmail.com>
2023-06-05 21:04:00 -0500

    se actualizo el nombre la tabla

--- app/Models/certificacion.php
+++ app/Models/certificacion.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class certificacion extends Model
+class caregarCertificados extends Model
 {
     use HasFactory;
-    protected $table='certificacion';
+    protected $table='caregarCertificados';
     protected $guarded=[];
 }
